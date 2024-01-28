3a48739cff14c19620886ed134b8f5f68b698020
guillermosifu <sifuentes1201@gmail.com>
2024-01-27 22:19:24 -0500

    name error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import RafaHeader from './Rafael/RafaHeder.jsx';
 import RafaOfertas from './Rafael/RafaOfertas.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RafaFooter from './Rafael/RafaFooter.jsx';
+import RafaFooter from './Rafael/rafaFooter.jsx';
+
 
 {
   /* The following line can be included in your src/index.js or App.js file */
